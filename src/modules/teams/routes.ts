dab6e26a68a6f621c75d93f2f848419fd77ca922
keerthi538 <keerthikv.dev@gmail.com>
2026-01-23 23:43:53 +0530

    feat: resolve teamRoutes 401 issue, add /teams/create endpoint

--- src/modules/teams/routes.ts
+++ src/modules/teams/routes.ts
@@ -1,24 +1,70 @@
-import type { FastifyInstance } from "fastify";
+import type { FastifyInstance, FastifyRequest } from "fastify";
 import { prisma } from "../../plugins/prisma";
+import { Role } from "../../../generated/prisma/enums";
 
 export async function teamsRoutes(fastify: FastifyInstance) {
-  fastify.get<{ Reply: unknown }>("/", async (request, reply) => {
-    if (!request.user?.id) {
-      reply.code(401);
-      return { error: "Unauthorized" };
-    }
-    try {
-      const memberships = await prisma.membership.findMany({
-        where: { userId: parseInt(request.user.id) },
-        include: {
-          team: true,
-        },
-      });
-      return memberships.map((membership) => membership.team);
-    } catch (error) {
-      fastify.log.error(error);
-      reply.code(500);
-      return { error: "Failed to fetch teams" };
-    }
-  });
+  fastify.get<{ Reply: unknown }>(
+    "/",
+    { preHandler: fastify.authenticate },
+    async (request, reply) => {
+      if (!request.user?.id) {
+        reply.code(401);
+        return { error: "Unauthorized" };
+      }
+
+      try {
+        const memberships = await prisma.membership.findMany({
+          where: { userId: request.user.id },
+          include: {
+            team: true,
+          },
+        });
+
+        return memberships.map((membership) => membership.team);
+      } catch (error) {
+        fastify.log.error(error);
+        reply.code(500);
+
+        return { error: "Failed to fetch teams" };
+      }
+    },
+  );
+
+  fastify.post<{ Body: { teamName: string } }>(
+    "/create",
+    { preHandler: fastify.authenticate },
+    async (request, reply) => {
+      if (!request.user?.id) {
+        reply.code(401);
+        return { error: "Unauthorized" };
+      }
+
+      try {
+        const teamName = request.body?.teamName;
+
+        if (typeof teamName !== "string" || teamName.trim() === "") {
+          reply.code(400);
+          return { error: "Invalid team name" };
+        }
+
+        const newTeam = await prisma.team.create({
+          data: {
+            name: teamName,
+            memberships: {
+              create: {
+                userId: request.user.id,
+                role: Role.ADMIN,
+              },
+            },
+          },
+        });
+
+        return newTeam;
+      } catch (error) {
+        fastify.log.error(error);
+        reply.code(500);
+        return { error: "Failed to create team" };
+      }
+    },
+  );
 }
